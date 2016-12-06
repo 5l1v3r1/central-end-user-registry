562a2aa4fe5e6982a5df38f381ae6eddfba708fa
The Mead <jasmead@gmail.com>
2016-12-06 09:44:31 -0600

    fix: Use Application Load Balancer (#23)
    * fix: Configure ALB hostname and hostPort
    
    * fix: Clean hostname

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,1 +1,7 @@
-module.exports = require('rc')('CREG', require('../../config/default.json'))
+const RC = require('rc')('CREG', require('../../config/default.json'))
+
+module.exports = {
+  DATABASE_URI: RC.DATABASE_URI,
+  HOSTNAME: RC.HOSTNAME.replace(/\/$/, ''),
+  PORT: RC.PORT
+}
